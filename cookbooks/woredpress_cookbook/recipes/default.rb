a42f6cba8b6e68446441ca6089e3a8f8563c6287
John Smith <john@example.com>
2015-12-01 11:13:21 +0530

    first commit

--- cookbooks/woredpress_cookbook/recipes/default.rb
+++ cookbooks/woredpress_cookbook/recipes/default.rb
@@ -45,23 +45,24 @@ package "php" do
   action :install
 end
 
-#  mysql2_chef_gem 'default' do
-#   action :install
-# end
+ mysql2_chef_gem 'default' do
+  action :install
+end
 
-# package "mysql-community-server" do
-#   action :install
-# end
+package "mysql-community-server" do
+  action :install
+end
 
-# service "mysqld" do
-#   action [:start, :enable]
-# end
+service "mysqld" do
+  action [:start, :enable]
+end
 
 
+#mkdir -p /var/www/html/
 
 path = Chef::Config[:file_cache_path]
 
-remote_file "/#{path}/['wordpress']['version'].tar.gz" do
+remote_file "/#{path}/latest.tar.gz" do
         source "http://wordpress.org/latest.tar.gz"
         action :create_if_missing
     end
@@ -69,8 +70,7 @@ remote_file "/#{path}/['wordpress']['version'].tar.gz" do
         bash "extract_wordpress" do
   cwd "/#{path}"
   code <<-EOH
-  mkdir -p /var/www/html/wordpress
-  tar -xzvf ['wordpress']['version'].tar.gz -C /var/www/html/wordpress
+  tar -xzvf latest.tar.gz -C /var/www/html/wordpress
   chown -R apache:apache /var/www/html/wordpress
   chmod -R 755 /var/www/html/wordpress
   mkdir -p /var/www/html/wordpress/wp-content/uploads
@@ -79,103 +79,48 @@ remote_file "/#{path}/['wordpress']['version'].tar.gz" do
 
 end
 
+bash "mysql_installation" do
+  code <<-EOH
+  mysql -e "update mysql.user set Password = Password('redhat') where User = 'root'"
+  mysql -e "drop user ''@'localhost'"
+  mysql -e "drop database test"
+  mysql -e "flush privileges"
+  EOH
+end
 
-# remote_file "/#{path}/drupal-#{node.drupal2.remote_file_v}.tar.gz" do
-#         source "http://ftp.drupal.org/files/projects/drupal-#{node.drupal2.remote_file_v}.tar.gz"
-#         action :create_if_missing
-# end
-
-# bash "extract_drupal" do
-#   cwd "/#{path}"
-#   code <<-EOH
-#   mkdir -p /var/www/html/
-#   tar -xzvf drupal-#{node.drupal2.remote_file_v}.tar.gz --strip-components=1 -C /var/www/html/
-#   chown -R apache:apache /var/www/html/
-#   chcon -R -t httpd_sys_content_rw_t /var/www/html/sites/
-#   EOH
-
-
-
-
-# execute 'configure-wordpress' do
-#   command "wp core config --path=#{ node['wordpress']['base_dir'] } --dbname=#{ node['wordpress']['db']['name'] } --dbuser=#{ node['wordpress']['db']['user'] } --dbpass=#{ node['wordpress']['db']['pass'] }"
-#   not_if "ls #{ node['wordpress']['base_dir'] }/wp-config.php"
-# end
-
-
-
-
-# mysql_service 'default' do
-#   port '3306'
-#   version '5.5'
-#   initial_root_password 'redhat'
-#   action [:create, :start]
-# end
-
-
-
-# bash "mysql_installation" do
-#   code <<-EOH
-#   mysql -e "update mysql.user set Password = Password('redhat') where User = 'root'"
-#   mysql -e "drop user ''@'localhost'"
-#   mysql -e "drop database wordpres"
-#   mysql -e "flush privileges"
-#   EOH
-# end
-
-
-# # bash "mysql -u root" do
-# # code <<-EOH
-# mysql -u root <<-EOF
-# UPDATE mysql.user SET Password=PASSWORD('redhat') WHERE User='root';
-# DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-# DELETE FROM mysql.user WHERE User='';
-# DELETE FROM mysql.db WHERE Db='wordpres' OR Db='wordpres\_%';
-# FLUSH PRIVILEGES;
-# EOF
-# EOH
-# end
-
-
-
-
-# bash "mysql_installation" do
-#   code <<-EOH
-#   mysql_secure_installation <<-EOF
-#   /n
-#   y/n
-#   connected/n
-#   connected/n
-#   y/n
-#   y/n
-#   y/n
-#   y/n
-#   EOF
-#   EOH
-# end
-
-
-
-
-
-# mysql_database node['wordpress']['db']['name'] do
-#   connection(
-#     :host     => '127.0.0.1',
-#     :username => node['wordpress']['db']['user'],
-#     :password => node['wordpress']['db']['pass']
+mysql_database node['wordpress']['db']['name'] do
+  connection(
+    :host     => '127.0.0.1',
+    :username => 'root',
+    :password => node['wordpress']['db']['pass']
    
-#     )
-#   action :create
-# end
+    )
+  action :create
+end
+
+mysql_database_user node['wordpress']['db']['user'] do
+  password node['wordpress']['db']['pass']
+  database_name node['wordpress']['db']['name']
+  host '%'
+  privileges [:all]
+  require_ssl true
+  action :grant
+end
+
+cookbook_file "/etc/httpd/conf/httpd.conf" do
+  source "virtual.conf.erb"
+  mode "644"
+  owner "root"
+  group "root"
+  action :create
+  notifies :restart, "service[httpd]"
+end
 
 
-# mysql_database_user node['wordpressdb']['db']['user'] do
-#   password node['wordpress']['db']['pass']
-#   database_name node['wordpress']['db']['name']
-#   host '%'
-#   privileges [:all]
-#   require_ssl true
-#   action :grant
-# end
-
-
+template "/var/www/html/wordpress/wp-config.php.erb" do
+  source "wp-config.php.erb"
+  mode "666"
+  owner "apache"
+  group "apache"
+  action :create
+end
